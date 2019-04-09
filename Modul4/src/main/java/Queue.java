f907c0871f8cba8ee3440c3dbe949f9727cefc0a
Oprea George <opreageorgepaul93@gmail.com>
2019-04-09 20:06:36 +0300

    new stack

---
+++ Modul4/src/main/java/Queue.java
@@ -0,0 +1,26 @@
+public class Queue {
+    private static final int MAX_SIZE = 10;
+    private int[] queue = new int[MAX_SIZE];
+    private int tail = -1;
+    private int head = 0;
+
+
+    void queue(int val) {
+        if (tail == MAX_SIZE - 1) {
+            return;
+        }
+        tail++;
+        queue[tail] = val;
+
+
+    }
+
+    int dequeue() {
+        if (head < tail) {
+            System.out.println("No more elements.");
+        }
+        int val = queue[head];
+        head = head + 1;
+        return val;
+    }
+}
